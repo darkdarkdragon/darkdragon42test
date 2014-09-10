122cfb5b25270add61e9de1e2d241a154a01d204
Ivan Tivonenko <darkdarkdragon@gmail.com>
2014-09-10 15:27:03 +0300

    ticket:3 contact form added done

---
+++ django_angular_backend/static/app/app.contactform.js
@@ -0,0 +1,45 @@
+'use strict';
+(function () {
+
+angular.module('app.contactform', ['ngRoute'])
+.controller('ContactForm', ['$http', '$routeParams', ContactForm])
+
+.config(['$routeProvider', function($routeProvider) {
+  $routeProvider.when('/contacts/:contactId', {
+    templateUrl: '/static/contactform.html',
+    controller: 'ContactForm',
+    controllerAs: 'contactForm'
+  });
+}]);
+
+
+
+function ContactForm($http, $routeParams) {
+    this.disabled = true;
+    this.contact = {};
+    if (!isNaN(parseInt($routeParams.contactId))) {
+        this.contact.id = parseInt($routeParams.contactId);
+        var self = this;
+        $http.get('/api/v1/contact/' + this.contact.id).success(function(data) {
+            if (data != null && data.id === self.contact.id) {
+                self.contact = data;
+                self.disabled = false;
+            }
+        }).error(function(data, status, headers, config) {
+            if (status == 404) {
+                self.disabled = false;
+            }
+        });
+    }
+
+    this.save = function() {
+        if (!this.disabled) {
+            var self = this;
+            $http.put('/api/v1/contact/' + self.contact.id, self.contact);
+        }
+    }
+
+}
+
+
+})();
