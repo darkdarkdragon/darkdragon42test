658270bae5e5e28426d7ddc12e5c1f4f2c43c19f
Ivan Tivonenko <darkdarkdragon@gmail.com>
2014-09-13 01:54:19 +0300

    ticket:1 changed package.json so 'npm test' will run tests once on PhantomJS - this is for Barista developers now must run 'npm run dev-test' in makefile test target run npm test

--- django_angular_backend/static/karma.barista.conf.js
+++ django_angular_backend/static/karma.barista.conf.js
@@ -8,7 +8,8 @@ module.exports = function(config){
       'app/bower_components/angular-route/angular-route.js',
       'app/bower_components/angular-resource/angular-resource.js',
       'app/bower_components/angular-mocks/angular-mocks.js',
-      'app/*.js'
+      'app/*.js',
+      'app/*.html'
     ],
 
     autoWatch : true,
@@ -21,12 +22,27 @@ module.exports = function(config){
             'karma-chrome-launcher',
             'karma-phantomjs-launcher',
             'karma-jasmine',
-            'karma-junit-reporter'
+            'karma-junit-reporter',
+            'karma-ng-html2js-preprocessor'
             ],
 
     junitReporter : {
       outputFile: 'test_out/unit.xml',
       suite: 'unit'
+    },
+
+    preprocessors: {
+      'app/*.html': 'ng-html2js'
+    },
+
+    ngHtml2JsPreprocessor: {
+      // If your build process changes the path to your templates,
+      // use stripPrefix and prependPrefix to adjust it.
+      stripPrefix: "app/",
+      prependPrefix: "/static/",
+
+      // the name of the Angular module to create
+      moduleName: "my.templates"
     }
 
   });
